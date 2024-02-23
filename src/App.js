7b328a52b881ec26da7a1228b98d14a1ef31894a
Rupesh <rupesh@Rupeshs-MacBook-Air.local>
2024-02-23 19:38:18 +0530

    client

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-// import React from 'react'
-// import { Route, Routes } from 'react-router-dom'
-// import Home from './Components/Home'
-// import Login from './Components/Login'
-// import Signup from './Components/SignUp'
-// import AddRestro from './Components/AddRestro'
-// import View from './Components/View'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import Login from './Components/Login'
+import Signup from './Components/SignUp'
+import AddRestro from './Components/AddRestro'
+import View from './Components/View'
 
 
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Routes>
-//         <Route path='/'       element={<Home/>}  />
-//         <Route  path='/login'   element={<Login/>}/>
-//         <Route  path='/signup'   element={<Signup/>}/>
-//         <Route  path='/addr'   element={<AddRestro/>}/>
-//         <Route  path='/view'   element={<View/>}/>
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/'       element={<Home/>}  />
+        <Route  path='/login'   element={<Login/>}/>
+        <Route  path='/signup'   element={<Signup/>}/>
+        <Route  path='/addr'   element={<AddRestro/>}/>
+        <Route  path='/view'   element={<View/>}/>
 
 
 
 
 
         
-//       </Routes>
+      </Routes>
 
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-import React from 'react'
-import ComA from './ComA'
-
-const App = () => {
-  let user='hello'
-  return (
-    <div>
-      <ComA />
     </div>
   )
 }
 
 export default App
+
+
+
+// import React from 'react'
+// import ComA from './ComA'
+
+// const App = () => {
+//   let user='hello'
+//   return (
+//     <div>
+//       <ComA />
+//     </div>
+//   )
+// }
+
+// export default App
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,9 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Container, Grid } from '@mui/material';
+import axios from 'axios'
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    passWord: ''
+ 
+  });
 
-export default Signup
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit =   async(e) => {
+    e.preventDefault();
+    // Add your signup logic here
+    let res=  await  axios.post('http://localhost:4000/api/users',formData)
+    console.log(res);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4" align="center">Sign Up</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="name"
+              variant="outlined"
+              fullWidth
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="PassWord"
+              variant="outlined"
+              fullWidth
+              type="password"
+              name="passWord"
+              value={formData.passWord}
+              onChange={handleChange}
+            />
+          </Grid>
+        
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+            >
+              Sign Up
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
+
+export default Signup;
